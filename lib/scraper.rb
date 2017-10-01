a69d3ba572bbd0269d8e2d6734e6bf4d976b2c72
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 20:15:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,8 +20,8 @@ class Scraper
       end
 
       def make_courses
-        self.get_courses.each do |course| 
-          course = Course.new 
+        self.get_courses.each do |course|
+          course = Course.new
           course.title = post.css("h2").text
           course.schedule = post.css("date").text
           course.description = post.css("p").text
